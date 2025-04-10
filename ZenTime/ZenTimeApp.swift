0468d4e774894f1e83d87aec769c6c7615add623
Aiden Baker <aidenbak9@gmail.com>
2025-04-10 09:27:01 -0500

    Initial Commit

---
+++ ZenTime/ZenTimeApp.swift
@@ -0,0 +1,17 @@
+//
+//  ZenTimeApp.swift
+//  ZenTime
+//
+//  Created by Aiden Baker on 4/10/25.
+//
+
+import SwiftUI
+
+@main
+struct ZenTimeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
