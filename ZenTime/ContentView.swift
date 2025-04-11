bd7b0712b7866671d1a4216a4a39f0909b4f8d07
Aiden Baker <aidenbak9@gmail.com>
2025-04-11 09:42:20 -0500

    First Commit

--- ZenTime/ContentView.swift
+++ ZenTime/ContentView.swift
@@ -10,12 +10,21 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("Welcome to My App!")
+                .font(.largeTitle)
+                .padding()
+            
+            Button(action: {
+                print("Button tapped!")
+            }) {
+                Text("Get Started")
+                    .font(.headline)
+                    .padding()
+                    .background(Color.blue)
+                    .foregroundColor(.white)
+                    .cornerRadius(10)
+            }
         }
-        .padding()
     }
 }
 
