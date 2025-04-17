6e2d77fb73f4e3e8ce369dc92d09f71818f3a580
Aiden Baker <aidenbak9@gmail.com>
2025-04-17 10:19:19 -0500

    Timer/Alarm Will Change this is base

--- ZenTime/ContentView.swift
+++ ZenTime/ContentView.swift
@@ -8,23 +8,89 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var timeRemaining = 60 // Timer value in seconds
+    @State private var isTimerRunning = false
+    @State private var timer: Timer?
+    
     var body: some View {
-        VStack {
-            Text("Welcome to My App!")
+        VStack(spacing: 30) {
+            Text("Timer")
                 .font(.largeTitle)
-                .padding()
+                .fontWeight(.bold)
             
-            Button(action: {
-                print("Button tapped!")
-            }) {
-                Text("Get Started")
-                    .font(.headline)
-                    .padding()
-                    .background(Color.blue)
-                    .foregroundColor(.white)
-                    .cornerRadius(10)
+            Text(timeString(time: timeRemaining))
+                .font(.system(size: 70, weight: .medium))
+                .frame(minWidth: 200)
+                .padding(.vertical, 20)
+            
+            HStack(spacing: 30) {
+                Button(action: {
+                    if isTimerRunning {
+                        pauseTimer()
+                    } else {
+                        startTimer()
+                    }
+                }) {
+                    Text(isTimerRunning ? "Pause" : "Start")
+                        .font(.title2)
+                        .fontWeight(.semibold)
+                        .frame(width: 100)
+                        .padding()
+                        .background(isTimerRunning ? Color.orange : Color.green)
+                        .foregroundColor(.white)
+                        .cornerRadius(10)
+                }
+                
+                Button(action: resetTimer) {
+                    Text("Reset")
+                        .font(.title2)
+                        .fontWeight(.semibold)
+                        .frame(width: 100)
+                        .padding()
+                        .background(Color.red)
+                        .foregroundColor(.white)
+                        .cornerRadius(10)
+                }
+            }
+            
+            Slider(value: Binding(
+                get: { Double(timeRemaining) },
+                set: { newValue in
+                    timeRemaining = Int(newValue)
+                }
+            ), in: 5...300, step: 5)
+            .padding()
+            .disabled(isTimerRunning)
+        }
+        .padding()
+    }
+    
+    func startTimer() {
+        isTimerRunning = true
+        timer = Timer.scheduledTimer(withTimeInterval: 1, repeats: true) { _ in
+            if timeRemaining > 0 {
+                timeRemaining -= 1
+            } else {
+                pauseTimer()
             }
         }
+    }
+    
+    func pauseTimer() {
+        isTimerRunning = false
+        timer?.invalidate()
+        timer = nil
+    }
+    
+    func resetTimer() {
+        pauseTimer()
+        timeRemaining = 60 // Reset to default value
+    }
+    
+    func timeString(time: Int) -> String {
+        let minutes = time / 60
+        let seconds = time % 60
+        return String(format: "%02d:%02d", minutes, seconds)
     }
 }
 
