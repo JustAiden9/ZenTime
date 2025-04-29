3895230b33567de88ba214b7f593cc3913bdce17
Aiden Baker <aidenbak9@gmail.com>
2025-04-29 09:40:17 -0500

    Milestone and Rewards file upload

---
+++ ZenTime/Milestone.swift
@@ -0,0 +1,19 @@
+//
+//  Milestone.swift
+//  ZenTime
+//
+//  Created by Aiden Baker on 4/23/25.
+//
+
+import SwiftUI
+
+struct Milestone: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Milestone()
+}
+
---
+++ ZenTime/Rewards.swift
@@ -0,0 +1,18 @@
+//
+//  Rewards.swift
+//  ZenTime
+//
+//  Created by Aiden Baker on 4/29/25.
+//
+
+import SwiftUI
+
+struct Rewards: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Rewards()
+}
